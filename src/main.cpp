b1420ff7c2900bb4979fd0c4a393707a20fa9943
uburuntu <bekbulatov.ramzan@ya.ru>
2017-04-22 20:14:33 +0300

    REF: indententions to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -34,33 +34,33 @@ using namespace TgBot;
 bool sigintGot = false;
 
 int main() {
-	Bot bot(API_TOKEN);
-	bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
-		bot.getApi().sendMessage(message->chat->id, "Hi!");
-	});
-	bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
-		printf("User wrote %s\n", message->text.c_str());
-		if (StringTools::startsWith(message->text, "/start")) {
-			return;
-		}
-		bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-	});
+    Bot bot(API_TOKEN);
+    bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
 
-	signal(SIGINT, [](int s) {
-		printf("SIGINT got");
-		sigintGot = true;
-	});
-	try {
-		printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+    signal(SIGINT, [](int s) {
+        printf("SIGINT got");
+        sigintGot = true;
+    });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
 
-		TgLongPoll longPoll(bot);
-		while (!sigintGot) {
-			printf("Long poll started\n");
-			longPoll.start();
-		}
-	} catch (exception& e) {
-		printf("error: %s\n", e.what());
-	}
+        TgLongPoll longPoll(bot);
+        while (!sigintGot) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (exception& e) {
+        printf("error: %s\n", e.what());
+    }
 
-	return 0;
+    return 0;
 }
