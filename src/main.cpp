2fa9987406361e29858a482cc0c6826f795fd646
uburuntu <bekbulatov.ramzan@ya.ru>
2017-04-23 18:06:28 +0300

    REF: update main.cpp style

--- src/main.cpp
+++ src/main.cpp
@@ -30,34 +30,47 @@
 
 bool sigintGot = false;
 
-int main() {
-    TgBot::Bot bot(API_TOKEN);
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
-    });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
-            return;
-        }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-    });
+int main()
+{
+  TgBot::Bot bot (API_TOKEN);
+  bot.getEvents().onCommand ("start", [&bot] (TgBot::Message::Ptr message)
+  {
+    bot.getApi().sendMessage (message->chat->id, "Hi!");
+  });
+  bot.getEvents().onAnyMessage ([&bot] (TgBot::Message::Ptr message)
+  {
+    printf ("User wrote %s\n", message->text.c_str());
 
-    signal(SIGINT, [](int) {
-        printf("SIGINT got");
-        sigintGot = true;
-    });
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+    if (StringTools::startsWith (message->text, "/start"))
+      {
+        return;
+      }
 
-        TgBot::TgLongPoll longPoll(bot);
-        while (!sigintGot) {
-            printf("Long poll started\n");
-            longPoll.start();
+    bot.getApi().sendMessage (message->chat->id, "Your message is: " + message->text);
+  });
+
+  signal (SIGINT, [] (int)
+  {
+    printf ("SIGINT got");
+    sigintGot = true;
+  });
+
+  try
+    {
+      printf ("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+
+      TgBot::TgLongPoll longPoll (bot);
+
+      while (!sigintGot)
+        {
+          printf ("Long poll started\n");
+          longPoll.start();
         }
-    } catch (std::exception& e) {
-        printf("error: %s\n", e.what());
+    }
+  catch (std::exception &e)
+    {
+      printf ("error: %s\n", e.what());
     }
 
-    return 0;
+  return 0;
 }
