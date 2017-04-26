b3725f571d458471c740fcfdda8bf2a892091d07
uburuntu <bekbulatov.ramzan@ya.ru>
2017-04-26 18:32:17 +0300

    ENH: update CMakeLists.txt and build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-mkdir --verbose ./build
-cd ./build
-cmake ..
-make -j4
+#!/bin/sh
+
+# Create build directory if needed and cd into it
+mkdir --verbose --parents ./build && cd ./build
+
+# Create makefile and compile project
+cmake .. && make -j4
