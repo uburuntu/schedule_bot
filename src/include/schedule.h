1eed02189cf7f907531dd71f87d4c63f0f56f321
uburuntu <bekbulatov.ramzan@ya.ru>
2017-05-17 02:47:17 +0300

    NEW: embedded schedules

--- src/include/schedule.h
+++ src/include/schedule.h
@@ -1,10 +1,45 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
 
-class schedule_t
+#include <string>
+#include <unordered_map>
+
+#include "include/event.h"
+#include "include/notifies.h"
+
+class schedule_interface
 {
   public:
-    schedule_t () {}
+    virtual ~schedule_interface () {}
+
+    // Initialize events from file or by other logic
+    virtual void init_schedule () = 0;
+
+    virtual void update_schedule () = 0;
+};
+
+class schedule_tsphere : public schedule_interface
+{
+  public:
+    schedule_tsphere () {}
+
+    virtual void init_schedule () {}
+    virtual void update_schedule () {}
+
+  private:
+    std::vector<event_ptr> all_exercises;
+};
+
+class schedule_mechmath : public schedule_interface
+{
+  public:
+    schedule_mechmath () {}
+
+    virtual void init_schedule () {}
+    virtual void update_schedule () {}
+
+  private:
+    std::vector<event_ptr> all_exercises;
 };
 
 #endif // SCHEDULE_H
